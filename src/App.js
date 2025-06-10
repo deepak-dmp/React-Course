22933d63c6f5af52cb0bfa0b2868a236f3e59ac2
Deepak Pathak <Deepakpathak.dmp83@gmail.com>
2025-06-11 04:40:45 +0530

    vido 6

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+   <>
+   <Navbar title={123} about='Help Me' />
+   </>
+  );
+}
+
+export default App;
