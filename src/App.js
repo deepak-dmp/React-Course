a2b6f661b8baca47d7ad8efaea91bd456e8ccf81
Deepak Pathak <Deepakpathak.dmp83@gmail.com>
2025-06-12 23:47:14 +0530

    video 7 adding text summary in TextFom.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 
 function App() {
   return (
    <>
-   <Navbar title={123} about='Help Me' />
+   <Navbar title="Textutils" about='About Me' />
+
+   <div className="container my-3">
+
+    <TextForm heading="Enter your text to analyze below" />
+   
+   </div>
+   
    </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-export default function TextForm() {
+export default function TextForm({heading="Enter Heading"}) {
+
+  const handleUpCLick = () =>{
+    // console.log("Upper case was clicked" + text)
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleOnChange = (event) =>{
+
+    // console.log("OnChange")
+    setText(event.target.value)
+  }
+
+  const [text,setText]=useState('Enter text here');
+
   return (
-    <div>
-      
+    <>
+    <div className="container">
+      <h1>{heading}</h1>
+     <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpCLick}>Convert to upper case</button>
     </div>
+
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+    </div>
+    </>
   )
 }
