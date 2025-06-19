1e57d9484658339c46d0a53748590844f456c23b
Deepak Pathak <Deepakpathak.dmp83@gmail.com>
2025-06-20 00:43:28 +0530

    video #13 (1):- added alert with basic message as a prop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState } from 'react';
 import './App.css';
 // import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -12,21 +13,25 @@ function App() {
     if(mode==="light")
     {
       setMode("dark")
+      document.body.style.backgroundColor="#222324"
       
     }
     else
     {
       setMode("light")
+      document.body.style.backgroundColor="white"
       
     }
   }
   return (
    <>
    <Navbar title="Textutils" about='About Me' mode={mode} toggleMode={toggleMode} />
-
+   
+   <Alert alert="hii my name is deepak" />
+   
    <div className="container my-3">
 
-    <TextForm heading="Enter your text to analyze below" />
+    <TextForm mode={mode} heading="Enter your text to analyze below" />
     {/* <About/> */}
    
    </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 
-export default function TextForm({heading="Enter Heading"}) {
+export default function TextForm({heading="Enter Heading", mode}) {
 
   const handleUpCLick = () =>{
     // console.log("Upper case was clicked" + text)
@@ -19,25 +19,33 @@ export default function TextForm({heading="Enter Heading"}) {
     setText(event.target.value)
   }
 
+  const handleClear = (event) =>{
+
+    // console.log("OnChange")
+    setText("")
+  }
+
   const [text,setText]=useState('');
 
   return (
-    <>
-    <div className="container">
+    <div className='container' style={{color : mode === "light" ? "#222324":"white"}}>
+    
+    <div className="container" >
       <h1>{heading}</h1>
      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : mode === "light" ? "white":"#222324",color : mode === "light" ? "#222324":"white"}} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpCLick}>Convert to upper case</button>
       <button className="btn btn-primary mx-2" onClick={handleLoCLick}>Convert to lower case</button>
+      <button className="btn btn-primary mx-2" onClick={handleClear}>Clear</button>
     </div>
 
     <div className="container my-3">
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p> {0.008* text.split(" ").length} Minutes to read</p>
-      <pre> {text} </pre>
+      <pre> {text.length>0?text:"Enter something to preview it here"} </pre>
     </div>
-    </>
+    </div>
   )
 }
