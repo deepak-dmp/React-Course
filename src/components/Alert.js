71195c3cd07250aa2d3b99ebc254fe90ed7c6481
Deepak Pathak <Deepakpathak.dmp83@gmail.com>
2025-06-20 01:39:22 +0530

    video #13 (4) : added time out alert function to fade alert after 1.5 second

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,6 @@ export default function Alert({alert}) {
     
       alert&&<div class={`alert alert-${alert.type} alert-dismissible fade show`} role="alert">
         <strong>{capitalze(alert.type)} :</strong> {alert.msg}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     
   )
