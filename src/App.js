7bccf4c7429fb758da67b689b79adb8445af67d6
deepak-dmp <93182222+deepak-dmp@users.noreply.github.com>
2025-06-25 05:20:37 +0530

    pulled in new project folder to commit new files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {useState } from 'react';
 import './App.css';
 import About from './components/About';
@@ -63,6 +64,30 @@ function App() {
    </>
   );
   
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> 16a281c (Initialize project using Create React App)
 }
 
 export default App;
